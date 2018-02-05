8119edc7128b0ad92276d68e2b6527b51b1ce9c7
root <root@Debian.local>
2018-02-05 18:17:15 +0300

    docker

---
+++ www/app/User.php
@@ -0,0 +1,9 @@
+<?php namespace App;
+
+class User
+{
+    public function getUser()
+    {
+        return 'get user method';
+    }
+}
---
+++ www/index.php
@@ -0,0 +1,9 @@
+<?php
+ # PDO için pgsql driver'ını yüklemek gerekiyor?
+ #$db = new PDO('pgsql:host=postgres;port=5432;dbname=test_docker;user=mehmetakkus;password=123123');
+
+ require 'vendor/autoload.php'; 
+
+ use App\User;
+ echo (new User())->getUser(); 
+?>
